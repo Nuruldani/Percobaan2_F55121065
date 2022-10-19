dc45f52a5b918382e8e033835688803d10bd7a75
rskrwnm <dwireski63@gmail.com>
2022-10-19 19:36:30 +0800

    Initial commit

---
+++ app/src/main/java/com/nurulrahmadanihsuryanto_f55121065/mylistview/f55121065.java
@@ -0,0 +1,14 @@
+package com.nurulrahmadanihsuryanto_f55121065.mylistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class f55121065 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_f55121065);
+    }
+}
